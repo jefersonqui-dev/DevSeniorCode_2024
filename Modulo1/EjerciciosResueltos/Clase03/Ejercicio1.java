0334adabd8888cd32d504fbd776e71bacfd6235c
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2024-10-31 16:26:36 -0500

    Actualizacion Folders

--- Modulo1/EjerciciosResueltos/Clase03/Ejercicio1.java
+++ Modulo1/EjerciciosResueltos/Clase03/Ejercicio1.java
@@ -1,17 +1,11 @@
+import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
-        byte operacionMatematica = 6 / 2 * (1 + 2);
-        System.out.printf("El Resultado es: %d", operacionMatematica);
+        System.out.print("Ingrese un Numero Real: ");
+        Scanner entrada = new Scanner(System.in);
 
-        // El resultado que se mostrara en Consola es 9, este es el valor correcto
-        //teniendo en cuenta la prioridad de los operadores, en este caso
-        // hay operadores de igual prioridad, pero por regla general se toma
-        // de izquierda a derecha.
-
-        //El caso particular que el resultado sea 1, esta relacionada como
-        // la calculadora puede manejar los operadores de manera diferente
-        // que para este caso seria el uso implícito del parentesis por lo
-        // que podriamos escribir (6/2)*(1+2) para obtener el mismo valor.
-   
+        var numero = entrada.nextFloat();
+        System.out.println("El numero ingresado es Real: " + numero);
+        entrada.close();
     }
 }
---
+++ Tutorias/Tutoria1/Monitoria/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // ejercicio1();
+        ejercicio2();
+    }
+    public static void ejercicio1() {
+        System.out.println("Prueba");
+        
+    }
+    public static void ejercicio2(){
+        System.out.println("Prueba funcion 2");
+    }
+}
