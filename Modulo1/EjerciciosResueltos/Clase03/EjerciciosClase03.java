baa38f376643a3221b82be981b9c353b6b16e7a3
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2024-11-11 21:25:36 -0500

    Actualizacion Clase 05 y clase 07

--- Modulo1/EjerciciosResueltos/Clase03/EjerciciosClase03.java
+++ Modulo1/EjerciciosResueltos/Clase03/EjerciciosClase03.java
@@ -115,6 +115,7 @@ public class EjerciciosClase03 {
         }
 
         // entrada.close();
+       
         DrawLine();
     }
 
---
+++ Modulo1/EjerciciosResueltos/Clase04/tutoria6.java
@@ -0,0 +1,19 @@
+public class tutoria6 {
+    public static void main(String[] args) {
+        //creacion de un array de enteros de amaño 5;
+        //int[] numeros = new int[5];
+        int[] numeroacceso = {1,2,3,4,5};       
+        
+        for(int i=0; i<numeroacceso.length; i++) {
+            System.out.println("Numero en la posicion: " + numeroacceso[i]);
+        }
+
+        int[][] matriz = new int[3][3];
+
+        int[][] matriz2 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+    }
+}
