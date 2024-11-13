6058d2554515a4fdcfd36d6d65db6eca29c2a853
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2024-11-13 16:01:43 -0500

    Actualizacion Ejercicios Clase 05

--- Modulo1/EjerciciosResueltos/Clase05/src/App.java
+++ Modulo1/EjerciciosResueltos/Clase05/src/App.java
@@ -2,35 +2,78 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        printWelcomeMessage(); 
+        printWelcomeMessage();
         operacionesConArrays();
+
     }
 
-    public static void operacionesConArrays() {
+    public static int[] operacionesConArrays() {
         Scanner getValue = new Scanner(System.in);
-        System.out.print("Operaciones de un Array: Ingrese el Tamaño: ");       
+        System.out.print("Operaciones de un Array: Ingrese el Tamaño: ");
         var arraySize = getValue.nextInt();
         int[] array = new int[arraySize];
-        for (int i = 0; i <= array.length-1; i++) {
-            System.out.printf("Ingrese el valor en la posicion [%d]: ",i);
+        for (int i = 0; i <= array.length - 1; i++) {
+            System.out.printf("Ingrese el valor en la posicion [%d]: ", i);
             array[i] = getValue.nextInt();
-            
         }
-        System.out.println(array);
-        
+        printArray(array);
+        addArray(array);
+        findMaxValue(array);
+        findMinValue(array);
+        invertArray(array);
+
         getValue.close();
+
+        return array;
     }
 
-    public static void findMaxValue() {
+    public static void printArray(int[] array) {
+        System.out.print("[ ");
+        for (int j = 0; j <= array.length - 1; j++) {
+            System.out.printf(array[j] + " ");
+        }
+        System.out.println("]");
+    }
+
+    public static void addArray(int[] array) {
+        int suma = 0;
+        for (int j = 0; j <= array.length - 1; j++) {
+            suma += array[j];
+        }
+        System.out.printf("\nLa suma es: %d\n", suma);
 
     }
 
-    public static void findMinValue() {
+    public static void findMaxValue(int[] array) {
+        int maxValue = array[0];
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.printf("El valor maximo es: %d\n", maxValue);
 
     }
 
-    public static void invertArray() {
+    public static void findMinValue(int[] array) {
+        int minValue = array[0];
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        System.out.printf("El valor minimo es: %d\n", minValue);
+    }
 
+    public static void invertArray(int[] array) {
+        int[] arrayInvert = new int[array.length];
+        int pos = array.length - 1;
+        for (int i = 0; i <= array.length - 1; i++) {
+            arrayInvert[i] = array[pos];
+            pos--;
+        }
+        printArray(arrayInvert);
+        DrawLine();
     }
 
     public static void sortInDescendingOrder() {
@@ -46,9 +89,14 @@ public class App {
     }
 
     private static void printWelcomeMessage() {
-        System.out.println("====================================");
-        System.out.println("| Ejercicios Resueltos Clase 05    |");
-        System.out.println("====================================");
+        System.out.println("========================================================================");
+        System.out.println("|                      Ejercicios Resueltos Clase 05                   |");
+        System.out.println("========================================================================");
         System.out.println();
+    }
+
+    private static void DrawLine() {
+        System.out.println();
+        System.out.println("========================================================================");
     }
 }
